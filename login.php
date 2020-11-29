8b8d0b9b9490e94f0d7c90581968c78c2e74fe23
x83005sz <samuel.zureick@studnet.manchester.ac.uk>
2020-11-29 16:29:34 +0000

    added login page

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login</title>
+		<style type=text/css>
+			#titleHeader {
+				border-style: solid;
+				display: inline-block;
+				text-align: center;
+
+			}
+
+		</style>
+	</head>
+	<body>
+		<div id='titleHeader'>
+		<h1>TITLE</h1>
+		<div>
+		<p>swag</p>
+
+		<form action="convert.php">
+			<input type='text' name='temp' id='temp'>
+			<input type='submit'value='Convert'>
+		</form>
+	</body>
+</html>
