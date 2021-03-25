02e61438603b54b784851cd218ccd51398465e9b
Sani <sani.anto@student.manchester.ac.uk>
2021-03-25 01:24:49 +0000

    Added gun reload

--- !game/scripts/GameScene.js
+++ !game/scripts/GameScene.js
@@ -34,11 +34,12 @@ class GameScene extends Phaser.Scene {
         this.gun
         this.crosshair
         this.projectiles
-        this.keys
         this.lastFiredTime = 0
+        this.lastReloadTime = 0
         this.stars
         this.health_pickups
         this.scoreText
+        this.ammoText
         this.toggleObjectives
         this.togglePause
         this.timerLabel
@@ -90,7 +91,7 @@ class GameScene extends Phaser.Scene {
         this.player.body.setCollideWorldBounds(true)
 
         // initialise gun //
-        this.gun = new Gun(this, 200, 120, 'gun')
+        this.gun = new Gun(this, 200, 120, 6, 'gun')
         // initialise crosshair //
         this.crosshair = new Crosshair(this, 200, 120, 'crosshair')
 
@@ -109,6 +110,13 @@ class GameScene extends Phaser.Scene {
         this.scoreText.setFontSize('15px')
         this.scoreText.setColor('#FFFFFF')
         this.scoreText.setBackgroundColor('#000000')
+
+        // Create Ammo Text //
+        this.ammoText = this.add.text(275, 286, 'Ammo: ' + this.gun.ammo)
+        this.ammoText.setFontSize('15px')
+        this.ammoText.setColor('#FFFFFF')
+        this.ammoText.setBackgroundColor('#000000')
+
 
         ///// Collectible Items /////
         // Coins //
@@ -136,12 +144,14 @@ class GameScene extends Phaser.Scene {
 
         // Add keys for inputs + projectiles //
         const {Q} = Phaser.Input.Keyboard.KeyCodes
+        const {R} = Phaser.Input.Keyboard.KeyCodes
         this.keys = this.input.keyboard.addKeys({
             space: 'SPACE',
             plus: 'PLUS',
             minus: 'MINUS',
             esc: 'ESC',
-            q: Q
+            q: Q,
+            r: R
         })
         this.projectiles = new Projectiles(this)
 
@@ -194,6 +204,7 @@ class GameScene extends Phaser.Scene {
         player.updateScore(-5);
         this.cameras.main.shake(15, 0.02)
         player.setTint(0xff0000)
+
         this.time.addEvent({
             delay: 500,
             callback: () => {
@@ -204,9 +215,7 @@ class GameScene extends Phaser.Scene {
             loop: false
         })
         enemy.explode()
-
         this.removeHealth()
-
     }
 
     // Player-Star Collision //
@@ -327,6 +336,7 @@ class GameScene extends Phaser.Scene {
     // general update class, ran with each game 'tick' //
     update(time, delta) {
         this.scoreText.setText('Score : ' + this.player.score);
+        this.ammoText.setText('Ammo: ' + this.gun.ammo);
 
         // game over if player's health is 0 //
         if (this.player.health <= 0) {
@@ -338,16 +348,29 @@ class GameScene extends Phaser.Scene {
         this.input.setDefaultCursor('url(teamAssets/blank_cursor.png), pointer') //hides cursor by making it a 1 pixel image
         var pointer = this.input.activePointer;
         if(pointer.leftButtonDown()){
-            if (time > this.lastFiredTime) {
-                this.lastFiredTime = time + 200 //set delay between projectile fire
-                this.projectiles.fireProjectile(this.player.x, this.player.y, this, pointer) //call func in "Projectile.js"
+            if (this.gun.ammo > 0) {
+                if (time > this.lastFiredTime) {
+                    this.lastFiredTime = time + 200 //set delay between projectile fire
+                    this.gun.shoot()
+                    this.projectiles.fireProjectile(this.player.x, this.player.y, this, pointer) //call func in "Projectile.js"
+                }
             }
         }
         this.gun.update(time, delta, pointer, this)
         this.crosshair.update(time, delta, pointer)
 
+        // reloads guns //
+        if (Phaser.Input.Keyboard.JustDown(this.keys.r)) {
+            if (!pointer.leftButtonDown()){
+                if (time > this.lastReloadTime) {
+                    this.lastReloadTime = time + 1000
+                    this.gun.reload()
+                }
+            }
+        }
+
         // Open objectives on key press "Q" //
-        if(this.keys.q.isDown) {
+        if (this.keys.q.isDown) {
             if (!this.toggleObjectives && !this.isPause) {
                 this.toggleObjectives = true;
                 this.clickObjective();
